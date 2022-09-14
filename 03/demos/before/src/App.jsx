4c338438a30972afbabc0209aae9d7d029b37596
Shelby Webb <shelbylyn.webb@gmail.com>
2022-09-13 21:30:05 -0600

    first commit

---
+++ 03/demos/before/src/App.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Footer from "./Footer";
+import Header from "./Header";
+import { getProducts} from "./services/productService";
+
+
+export default function App() {
+  const [size, setSize] = useState("");
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts("shoes").then((response) => setProducts(response));
+  }, []);
+
+  // const state = useState("");
+  // const size = state[0];
+  // const setSize = state[1];
+
+  function renderProduct(p) {
+    return (
+      <div key={p.id} className="product">
+        <a href="/">
+          <img src={`/images/${p.image}`} alt={p.name} />
+          <h3>{p.name}</h3>
+          <p>${p.price}</p>
+        </a>
+      </div>
+    );
+  }
+
+  const filteredProducts = size 
+    ? products.filter((p) => p.skus.find((s) => s.size === parseInt(size)))
+    : products;
+
+  return (
+    <>
+      <div className="content">
+        <Header />
+        <main>
+          <section id="filters">
+            <label htmlFor="size">Filter by Size:</label>{" "}
+            <select id="size" value={size} onChange={(e) => setSize(e.target.value)}>
+              <option value="">All sizes</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+            </select>
+            { size && <h2>Found {filteredProducts.length} items</h2>}
+          </section>
+          <section id="products">{filteredProducts.map(renderProduct)}</section>
+        </main>
+      </div>
+      <Footer />
+    </>
+  );
+}
